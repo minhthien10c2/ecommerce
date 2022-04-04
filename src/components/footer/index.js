951ff613af32dfb4adbee43ded389912264ab224
minhthien10c2 <minhthien10c2@gmail.com>
2022-04-04 11:22:45 +0700

    footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,104 @@
+import {
+  FooterContainer,
+  AboutUs,
+  FooterContent,
+  Copyright,
+  FooterTop,
+} from "./style/styled";
+import { FaFacebookF, FaInstagram, FaYoutube, FaTwitter } from "react-icons/fa";
+
+const Header = (props) => {
+  return (
+    <>
+      <FooterContainer>
+        <FooterTop>
+          <AboutUs>
+            <img
+              src="https://png.pngtree.com/png-clipart/20190515/original/pngtree-coffee-time-png-image_3626459.jpg"
+              alt="https://png.pngtree.com/png-clipart/20190515/original/pngtree-coffee-time-png-image_3626459.jpg"
+            />
+            <a class="about-us" href="#">
+              Về chúng tôi
+            </a>
+            <a class="blog" href="#">
+              Blog
+            </a>
+            <ul>
+              <li>
+                <FaFacebookF className="icon-facebook" />
+              </li>
+              <li>
+                <FaInstagram className="icon-instagram" />
+              </li>
+              <li>
+                <FaYoutube className="icon-youtube" />
+              </li>
+              <li>
+                <FaTwitter className="icon-twitter" />
+              </li>
+            </ul>
+          </AboutUs>
+          <FooterContent>
+            <h3>Dịch vụ khách hàng</h3>
+            <ul>
+              <li>
+                <a href="#">Phương thức thanh toán</a>
+              </li>
+              <li>
+                <a href="#">Đổi trả hàng</a>
+              </li>
+              <li>
+                <a href="#">Phí vận chuyển</a>
+              </li>
+              <li>
+                <a href="#">Các điều khoản và điều kiện</a>
+              </li>
+            </ul>
+          </FooterContent>
+          <FooterContent>
+            <h3>Tài Khoản</h3>
+            <ul>
+              <li>
+                <a href="#">Đăng ký </a> &nbsp; / &nbsp;
+                <a href="#">Đăng nhập </a>
+              </li>
+              <li>
+                <a href="#">Giỏ hàng</a>
+              </li>
+              <li>
+                <a href="#">Yêu thích</a>
+              </li>
+              <li>
+                <a href="#">Tra cứu đơn hàng</a>
+              </li>
+            </ul>
+          </FooterContent>
+          <FooterContent>
+            <h3>Liên hệ</h3>
+            <ul>
+              <li>
+                <a href="#">theme@gmail.com </a>
+              </li>
+              <li>
+                <a href="#">1234567890 </a>
+              </li>
+            </ul>
+          </FooterContent>
+        </FooterTop>
+        <Copyright>
+          <hr />
+          <div class="copyright-container">
+            <p>Powered by ...</p>
+            <div class="payment">
+              <img src="" alt="" srcset="" />
+              <img src="" alt="" srcset="" />
+              <img src="" alt="" srcset="" />
+            </div>
+          </div>
+        </Copyright>
+      </FooterContainer>
+    </>
+  );
+};
+
+export default Header;
